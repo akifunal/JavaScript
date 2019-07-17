06199f7c62996d056d8a79fb700dc0c46704bbab
akif095 <akifunal05@gmail.com>
2019-07-17 14:14:12 +0300

    Message Mixer

---
+++ Message Mixer Module/message.js
@@ -0,0 +1,15 @@
+import {countCharacter, capitalizeFirstCharacterOfWords, reverseWord, reverseAllWords, replaceFirstOccurence, replaceAllOccurrences, encode, palindrome, pigLatin} from './messageMixer';
+
+function displayMessage() {
+  console.log(countCharacter("What is the color of the sky?", "t"));
+  console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+  console.log(reverseWord("What is the color of the sky?"));
+  console.log(reverseAllWords("What is the color of the sky?"));
+  console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+  console.log(encode("What is the color of the sky?"));
+  console.log(palindrome('Mein Vorname ist Akif. Ich bin veirundzwanzig Jahre alt.'));
+  console.log(pigLatin('Ich lebe in Istanbul', ',m'));
+}
+
+displayMessage();
+
