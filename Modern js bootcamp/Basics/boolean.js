f046f6f618442b69d9fbe7c47a6796b88d7c4ebb
akif095 <akifunal05@gmail.com>
2019-07-19 14:47:34 +0300

    boolean example

---
+++ Modern js bootcamp/Basics/boolean.js
@@ -0,0 +1,10 @@
+// Create age set to your age
+let age = 24;
+// Calculate is child - if they are 7 or under
+let isChild = age <= 7;
+// Calculate is senior - if they are 65 or older
+let isSenior = age >= 65;
+// Print is child value
+console.log('Are u a child ' + isChild);
+// Print is senior value
+console.log('Are u a senior '+ isSenior);
