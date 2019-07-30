eafd2f0f8051b32f1f8f71d8aecec2ddf7799d63
akif095 <akifunal05@gmail.com>
2019-07-30 21:54:58 +0300

    DOM manipulation

---
+++ Modern js bootcamp/notes-app/notes-app.js
@@ -0,0 +1,14 @@
+//DOM -Document Object Model
+
+//Query and remove
+// const p = document.querySelector('p');
+// p.remove();
+
+
+//Query and remove all
+const ps = document.querySelectorAll('p');
+
+ps.forEach((p) => {
+    console.log(p.textContent);
+    //p.remove();
+});
