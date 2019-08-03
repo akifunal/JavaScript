3378de99497f51c89ffea5c99f6ed772c0004628
akif095 <akifunal05@gmail.com>
2019-08-03 13:09:03 +0300

    todo filter challenge

--- Modern js bootcamp/todo-app/todo-app.js
+++ Modern js bootcamp/todo-app/todo-app.js
@@ -15,25 +15,49 @@ const todos = [{
     completed: true
 }];
 
-const incompleteTodos = todos.filter((todo) => {
-    return !todo.completed;
-})
+const filters = {
+    searchText: ''
+};
 
-const summary = document.createElement('h2');
-summary.textContent = `You have ${incompleteTodos.length} todos left.`;
-document.querySelector('body').appendChild(summary);
-todos.forEach((todo) => {
-    const p = document.createElement('p');
-    p.textContent = todo.text;
-    document.querySelector('body').appendChild(p);
-})
+const renderTodos = (todos, filters) => {
+    const filteredTodos = todos.filter((todo) => {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+    });
+
+    const incompleteTodos = filteredTodos.filter((todo) => {
+        return !todo.completed;
+    })
+    
+    document.querySelector('#todos').innerHTML = '';
+
+    const summary = document.createElement('h2');
+    summary.textContent = `You have ${incompleteTodos.length} todos left.`;
+    document.querySelector('#todos').appendChild(summary);
+
+    filteredTodos.forEach((todo) => {
+        const todoEl = document.createElement('p');
+        todoEl.textContent = todo.text;
+        document.querySelector('#todos').appendChild(todoEl);
+    });
+}
+
+renderTodos(todos, filters);
+
+
 
 // Listen for new todo creation
-document.querySelector('#add-todo').addEventListener('click', (e) => {console.log('Add a new todo...');})
+document.querySelector('#add-todo').addEventListener('click', (e) => {
+    console.log('Add a new todo...');
+})
 
 
 // Listen for todo text change
 document.querySelector('#new-todo-text').addEventListener('input', (e) => {
-    console.log(e.target.value); 
+    console.log(e.target.value);
 })
 
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    filters.searchText = e.target.value;
+    renderTodos(todos, filters);
+})
