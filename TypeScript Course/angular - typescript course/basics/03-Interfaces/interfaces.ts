337321150d192c9c957716e957f5190fd5cc55dc
akif095 <akifunal05@gmail.com>
2019-09-16 13:27:19 +0300

    typescript example

--- TypeScript Course/angular - typescript course/basics/03-Interfaces/interfaces.ts
+++ TypeScript Course/angular - typescript course/basics/03-Interfaces/interfaces.ts
@@ -10,22 +10,32 @@ interface User {
     confirmPassword?: string; // Optional property => Does not have to be implemented
 }
 
-let user:User;
+let user: User;
 
 // This value does not satisfy the interface => Compilation error
 // user = { anything: 'anything', anynumber: 5};
 
 // This value does satisfy the interface
-user = {username: 'max', password: 'supersecret'};
+user = { username: 'max', password: 'supersecret' };
 
 // Interfaces can also contain functions (without the function body - as it only is a blueprint/ requirement)
 
 interface CanDrive {
-    accelerate(speed:number): void;
+    accelerate(speed: number): void;
 }
 
-let car:CanDrive = {
-    accelerate: function (speed:number) {
+let car: CanDrive = {
+    accelerate: function (speed: number) {
         // ...
     }
 };
+
+interface CanUDrive {
+    stop(speed: number): void;
+}
+
+let myCar: CanUDrive = {
+    stop: function (test: number) {
+
+    }
+}
