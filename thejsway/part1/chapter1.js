33dd96397f3c3317804224b08be56366c9869424
akif095 <akifunal05@gmail.com>
2020-02-23 02:49:06 +0300

    old chapter

---
+++ thejsway/part1/chapter1.js
@@ -0,0 +1,18 @@
+// Presentation
+
+let name = "Baptiste";
+let age = 40;
+console.log(`His name is ${name} and his age is ${age}.`);
+
+// Minimalistic calculator
+
+console.log(6 + 3);
+console.log(6 - 3);
+console.log(6 * 3);
+console.log(6 / 3);
+
+// Values prediction
+
+console.log(4 + 5); // 9
+console.log("4 + 5"); // 4 + 5
+console.log("4" + "5"); // 45   
