a12454abb488406a3fce1f943ecc1cb6cde24f8d
akifunal <akifunal05@gmail.com>
2021-01-11 11:41:46 +0300

    js.info day 1

---
+++ Javascript Info/1 - Js language/2 - Js Fundamentals/4 - Variables/script.js
@@ -0,0 +1,17 @@
+//! Working with variables
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert(must output“ John”).
+
+let name = "John";
+let admin = name;
+
+alert(admin);
+
+//! Giving the right name
+// Create a variable with the name of our planet. How would you name such a variable ?
+// Create a variable to store the name of a current visitor to a website.How would you name that variable ?
+
+let ourPlanetName = "Earth";
+let currentUserName = "Akif";
