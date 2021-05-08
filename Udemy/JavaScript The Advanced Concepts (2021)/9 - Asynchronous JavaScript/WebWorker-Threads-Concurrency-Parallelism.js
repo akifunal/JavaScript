e9092a21136ccb5fabf1d92a622340f192ea960c
akifunal <akifunal05@gmail.com>
2021-05-09 00:43:19 +0300

    modules section

--- Udemy/JavaScript The Advanced Concepts (2021)/9 - Asynchronous JavaScript/WebWorker-Threads-Concurrency-Parallelism.js
+++ Udemy/JavaScript The Advanced Concepts (2021)/9 - Asynchronous JavaScript/WebWorker-Threads-Concurrency-Parallelism.js
@@ -1,6 +1,6 @@
 const worker = new Worker("worker.js");
 worker.postMessage("Helloooo");
 
-//Cache Storage - Cache API - Web worker - window.caches
+// Cache Storage - window.caches - Cache API - Web worker
 // localStorage - window.localStorage - Web Storage API
 // sessionStorage - window.sessionStorage - Web Storage API
